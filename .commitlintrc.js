69e0a9837dffbdf3a914e5d741c3b40efd1e6694
thomas-ballo <liurongliang@balloai.com>
2021-12-28 14:52:18 +0800

    🔧 chore: 换成.commitlintrc.js的方式配置

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+
+// 具体的 规则配置项 https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+// 配置示例 https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+
+module.exports = {
+	extends: [
+		"gitmoji"
+	],
+	// parserPreset: 'conventional-changelog-conventionalcommits',
+	// rules: {}
+}
